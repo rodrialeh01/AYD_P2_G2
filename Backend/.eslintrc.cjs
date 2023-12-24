d589506f29b505f4ec52401412151e32e5f358f4
Rodri Hernández <2823897250101@ingenieria.usac.edu.gt>
2023-12-23 21:44:44 -0600

    Pipeline full

---
+++ Backend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['standard'],
+  env: {
+    node: true,
+    es6: true
+  },
+  rules: {
+    quotes: 'warn',
+    semi: 'warn',
+    'no-multiple-empty-lines': 'warn',
+    'eol-last': 'warn',
+    indent: 'warn',
+    'padded-blocks': 'warn',
+    'no-trailing-spaces': 'warn',
+    'key-spacing': 'warn',
+    'no-multi-spaces': 'warn',
+    'keyword-spacing': 'warn',
+    camelcase: 'warn',
+    'object-curly-spacing': 'warn',
+    'brace-style': 'warn',
+    'spaced-comment': 'warn',
+    'space-before-function-paren': 'warn',
+    'comma-dangle': 'warn'
+  }
+}
