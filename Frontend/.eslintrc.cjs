c89525fd8e2b1f793433dd04bf0d6f2aef5b7a07
Rodri Hernández <2823897250101@ingenieria.usac.edu.gt>
2023-12-26 18:26:46 -0600

    Cambios para bien en las pruebas y cambios de CI

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -8,13 +8,17 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 2022, // Cambiado a un valor específico para mayor claridad
+    sourceType: 'module',
+  },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'cypress'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'cypress/no-unnecessary-waiting': 'off',
   },
-}
+};
